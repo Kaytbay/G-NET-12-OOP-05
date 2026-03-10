cc4ff06964ae8693aff8befc58911a53652843d5
Kaytbay <mohamedkaytbay@gmail.com>
2026-03-10 14:24:06 +0200

    Part 1

--- oop 5/Program.cs
+++ oop 5/Program.cs
@@ -4,7 +4,100 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part1:
+
+            #region Q01:
+
+            /*Q1 : What is an interface in C#?
+             * Why do we use interfaces instead of depending on concrete classes directly? 
+             * Mention at least three benefits of using interfaces.
+             */
+
+            // contract that a class must follow with no impelemntation
+            //Enable multiple inheritance ,Enable polymorphism without inheritance, loose coupling & testability
+
+
+
+
+            #endregion
+
+
+
+            #region Q02 :
+
+
+            //a) What is the problem with this design? Both interfaces have a method called Greet() — how does the class handle it currently?
+
+            //    the both have the same Greet , the class use the one Greet to the two interfaces
+
+
+            //b) How would you fix this so IEnglishSpeaker.Greet() says "Hello" and IArabicSpeaker.Greet() says "Ahlan"? What is this technique called?
+
+            // by using the explicit implementation
+
+
+            //c) After applying your fix, can you call Greet() directly on a Translator object (e.g. translator.Greet())? Why or why not? How do you call each version?
+
+            // no i can only use them by an interface reference , like : englishSpeaker.Greet();
+
+            #endregion
+
+
+            #region Q03:
+
+            /*
+             Q3 : Explain the difference between a shallow copy and a deep copy.
+            When would you use each one? 
+            What is the risk of using a shallow copy when the object has reference-type fields?
+
+             */
+
+            // shallow make  a copy but the reference shared the same field , deep copy make a duplicate place
+
+            // use shallow if the performance is important and if there is no reference 
+            // use deep copy if the modifications should not affect original
+
+            // the risk that the two share the same reference and the changes will affect both
+
+
+
+
+
+
+            #endregion
+
+
+            #region Q04:
+            /*
+               class Department { public string Name; }
+                class Employee
+                {
+                    public string Title;
+                    public Department Dept;
+                    public Employee ShallowCopy() => (Employee)this.MemberwiseClone();
+                }
+
+                var e1 = new Employee { Title = "Dev", Dept = new Department { Name = "IT" } };
+                var e2 = e1.ShallowCopy();
+                e2.Title = "QA";
+                e2.Dept.Name = "Testing";
+
+                Console.WriteLine($"{e1.Title} - {e1.Dept.Name}");
+                Console.WriteLine($"{e2.Title} - {e2.Dept.Name}");
+             */
+
+            // e1.Title : Dev     e1.Dept.Name : Testing
+
+            // e2.Title : QA      e2.Dept.Name : Testing
+
+            // the title is string with is immutable so when modifu will create new one
+            // the dept.name is refernce wich the change will affect both 
+
+
+
+            #endregion
+
+            #endregion
         }
     }
 }
